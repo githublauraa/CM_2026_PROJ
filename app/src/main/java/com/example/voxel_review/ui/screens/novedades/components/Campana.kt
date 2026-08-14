06a2d09b41389d986856df83b977d4e740dd6eaa
Jose Cepeda <josecepeda@MacBook-Air-de-Jose-6.local>
2026-08-14 12:35:41 -0500

    Arreglo string campana

--- app/src/main/java/com/example/voxel_review/ui/screens/novedades/components/Campana.kt
+++ app/src/main/java/com/example/voxel_review/ui/screens/novedades/components/Campana.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.Image
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.size
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.voxel_review.R
 
@@ -14,7 +15,7 @@ fun Campana(
 ) {
     Image(
         painter = painterResource(R.drawable.campana_notificaciones),
-        contentDescription = "Notificaciones",
+        contentDescription = stringResource(R.string.notificaciones),
         modifier = modifier.size(27.dp)
     )
 }
