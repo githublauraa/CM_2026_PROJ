d26e113094872056cd5140739f3e91d7a08c2d23
Jose Cepeda <josecepeda@MacBook-Air-de-Jose-6.local>
2026-08-27 23:46:33 -0500

    arreglo en pantalla ranking con state hosting

--- app/src/main/java/com/example/voxel_review/ui/screens/rankings/RankingsScreen.kt
+++ app/src/main/java/com/example/voxel_review/ui/screens/rankings/RankingsScreen.kt
@@ -1,48 +1,50 @@
 package com.example.voxel_review.ui.screens.rankings
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import com.example.voxel_review.ui.utils.FondoPantalla
-import com.example.voxel_review.ui.screens.rankings.components.MensajeSuperior
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-import com.example.voxel_review.ui.screens.rankings.components.Estrella
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.material3.Text
-import androidx.compose.ui.graphics.Color
+import com.example.voxel_review.data.infoRanking.ListaRanking
+import com.example.voxel_review.data.infoRanking.RankingUsuario
 import com.example.voxel_review.ui.screens.rankings.components.BotonScreen
+import com.example.voxel_review.ui.screens.rankings.components.Estrella
+import com.example.voxel_review.ui.screens.rankings.components.MensajeSuperior
+import com.example.voxel_review.ui.screens.rankings.components.RankingCard
 import com.example.voxel_review.ui.theme.VoxelBackground
 import com.example.voxel_review.ui.theme.VoxelPrimary
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.Arrangement
-import com.example.voxel_review.data.infoRanking.ListaRanking
-import com.example.voxel_review.ui.screens.rankings.components.RankingCard
-import androidx.compose.foundation.lazy.items
-import com.example.voxel_review.ui.utils.DiscoverTab
 import com.example.voxel_review.ui.utils.DiscoverBottomNavigationBar
+import com.example.voxel_review.ui.utils.DiscoverTab
+import com.example.voxel_review.ui.utils.FondoPantalla
+
 
 @Composable
 fun RankingContent(
+    rankingUsuarios: List<RankingUsuario>,
     modifier: Modifier = Modifier
 ) {
+
     Box(
         modifier = modifier.fillMaxSize()
-    ){
+    ) {
+
         MensajeSuperior(
             modifier = Modifier.align(Alignment.TopStart)
         )
+
         Estrella(
             modifier = Modifier.align(Alignment.TopEnd)
         )
-        Spacer(modifier = Modifier.height(10.dp))
 
         BotonScreen(
             textoBoton = "Críticos",
@@ -50,28 +52,39 @@ fun RankingContent(
             colorTexto = Color.Gray,
             colorBorde = VoxelPrimary,
             onClick = {},
-            modifier = Modifier.align(Alignment.TopStart).padding(top = 70.dp, start = 18.dp)
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(
+                    top = 70.dp,
+                    start = 18.dp
+                )
         )
+
         BotonScreen(
             textoBoton = "Juegos",
             colorFondo = VoxelBackground,
             colorTexto = Color.Gray,
             colorBorde = Color.Gray,
             onClick = {},
-            modifier = Modifier.align(Alignment.TopStart).padding(top = 70.dp, start = 125.dp)
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(
+                    top = 70.dp,
+                    start = 125.dp
+                )
         )
-
-        Spacer(modifier = Modifier.height(10.dp))
 
         Text(
             text = "TOP CRÍTICOS",
             color = Color.Gray,
             fontSize = 16.sp,
-            modifier = Modifier.align(Alignment.TopStart).padding(top = 140.dp, start = 18.dp)
-
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(
+                    top = 140.dp,
+                    start = 18.dp
+                )
         )
-
-        Spacer(modifier = Modifier.height(10.dp))
 
         LazyColumn(
             modifier = Modifier
@@ -84,7 +97,7 @@ fun RankingContent(
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
 
-            items(ListaRanking.rankingUsuarios) { usuario ->
+            items(rankingUsuarios) { usuario ->
 
                 RankingCard(
                     posicion = usuario.posicion,
@@ -94,21 +107,24 @@ fun RankingContent(
                 )
             }
         }
-
-
     }
 }
 
+
 @Composable
 fun RankingsScreen(
+    rankingUsuarios: List<RankingUsuario>,
     modifier: Modifier = Modifier
 ) {
+
     Box(
         modifier = modifier.fillMaxSize()
     ) {
+
         FondoPantalla()
 
         RankingContent(
+            rankingUsuarios = rankingUsuarios,
             modifier = Modifier.padding(bottom = 70.dp)
         )
 
@@ -120,9 +136,12 @@ fun RankingsScreen(
     }
 }
 
-@Preview
+
+@Preview(showBackground = true)
 @Composable
 fun RankingScreenPreview() {
-    RankingsScreen()
-}
 
+    RankingsScreen(
+        rankingUsuarios = ListaRanking.rankingUsuarios
+    )
+}
