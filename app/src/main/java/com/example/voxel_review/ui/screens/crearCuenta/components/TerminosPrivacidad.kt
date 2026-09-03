c4ff636ec1fde6c154cedb00db2566a5928a68f7
Jose Cepeda <josecepeda@MacBook-Air-de-Jose-6.local>
2026-09-02 22:44:24 -0500

    mvvm en pantalla crear cuenta

---
+++ app/src/main/java/com/example/voxel_review/ui/screens/crearCuenta/components/TerminosPrivacidad.kt
@@ -0,0 +1,78 @@
+package com.example.voxel_review.ui.screens.crearCuenta.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import com.example.voxel_review.ui.theme.VoxelPrimary
+import com.example.voxel_review.ui.theme.VoxelTextSecondary
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.material3.Text
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TerminosPrivacidad(
+    aceptado: Boolean,
+    onAceptadoChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+
+        Checkbox(
+            checked = aceptado,
+            onCheckedChange = onAceptadoChange,
+            colors = CheckboxDefaults.colors(
+                checkedColor = VoxelPrimary,
+                uncheckedColor = VoxelPrimary,
+                checkmarkColor = Color.Black
+            )
+        )
+
+        Text(
+            buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = VoxelTextSecondary
+                    )
+                ) {
+                    append("Acepto los ")
+                }
+
+                withStyle(
+                    style = SpanStyle(
+                        color = VoxelPrimary,
+                        textDecoration = TextDecoration.Underline
+                    )
+                ) {
+                    append("Términos de Servicio")
+                }
+
+                withStyle(
+                    style = SpanStyle(
+                        color = VoxelTextSecondary
+                    )
+                ) {
+                    append(" y Privacidad")
+                }
+            },
+            fontSize = 10.sp
+        )
+    }
+}
+
+@Composable
+fun TerminosPrivacidadPreview(){
+    TerminosPrivacidad(
+        aceptado = true,
+        onAceptadoChange = {}
+    )
+}
